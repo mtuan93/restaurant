5f745c88e6555612ba45f03cf70b9338dfd7015c
Tuan Nguyen <mtuan93@gmail.com>
2015-05-14 17:07:42 +0000

    Initialize

---
+++ routes/users.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+/* GET /users/view */
+router.get('/view', function(req, res, next) {
+  res.send('user view');
+});
+module.exports = router;
